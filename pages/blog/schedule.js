f237b44a6b4327ca5f2ca4dbe659c6ff5f4f6873
itc-s23027 <s23027@std.it-college.ac.jp>
2024-02-09 02:55:44 +0000

    ch7

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -4,15 +4,16 @@ import Container from 'components/container'
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
-      <h1>{title}</h1>
+      <h1>記事のタイトル</h1>
     </Container>
   )
 }
+export default Schedule
 
 export async function getStaticProps () {
   const slug = 'schedule'
-  const post = await getPostBySlug(slug)
 
+  const post = await getPostBySlug(slug)
   return {
     props: {
       title: post.title,
@@ -23,5 +24,3 @@ export async function getStaticProps () {
     }
   }
 }
-
-export default Schedule
